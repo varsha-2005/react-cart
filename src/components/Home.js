9dff5053b9a6674e8b6c10f5ead18a8e71a533d6
Ashwin19v <ashwinash190105@gmail.com>
2024-08-03 17:23:37 +0530

    done

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,17 +2,35 @@ import React, { useState } from "react";
 import data from "../assests/products.json";
 import Product from "./Product";
 import "./Home.css";
+
 const Home = ({ cart, setCart, favorites, setFavorites }) => {
   const [search, setSearch] = useState("");
+  const [sortCriteria, setSortCriteria] = useState("");
   const [products] = useState(data);
-  const filteredProducts = products.filter((product) =>
-    product.name.toLowerCase().includes(search.toLowerCase())
+
+  const handleSort = (products) => {
+    switch (sortCriteria) {
+      case "priceLowToHigh":
+        return products.sort((a, b) => a.amt - b.amt);
+      case "priceHighToLow":
+        return products.sort((a, b) => b.amt - a.amt);
+      case "alphabetical":
+        return products.sort((a, b) => a.name.localeCompare(b.name));
+      default:
+        return products;
+    }
+  };
+
+  const filteredProducts = handleSort(
+    products.filter((product) =>
+      product.name.toLowerCase().includes(search.toLowerCase())
+    )
   );
 
   return (
     <>
       <form className="searchForm" onSubmit={(e) => e.preventDefault()}>
-        <label htmlFor="search"></label>
+        <label htmlFor="search">Search Items</label>
         <input
           id="search"
           role="searchbox"
@@ -20,7 +38,18 @@ const Home = ({ cart, setCart, favorites, setFavorites }) => {
           type="text"
           value={search}
           onChange={(e) => setSearch(e.target.value)}
-        ></input>
+        />
+        <label htmlFor="sort">Sort By:</label>
+        <select
+          id="sort"
+          value={sortCriteria}
+          onChange={(e) => setSortCriteria(e.target.value)}
+        >
+          <option value="">None</option>
+          <option value="priceLowToHigh">Price: Low to High</option>
+          <option value="priceHighToLow">Price: High to Low</option>
+          <option value="alphabetical">Alphabetical</option>
+        </select>
       </form>
       <div className="product-container">
         {filteredProducts.length > 0 ? (
