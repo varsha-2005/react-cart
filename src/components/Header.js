d2dc010804f3cad0f8e580fd39b910ef9128c46b
Ashwin19v <ashwinash190105@gmail.com>
2024-08-06 23:20:21 +0530

    c

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,23 +5,6 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBurger } from "@fortawesome/free-solid-svg-icons";
 
 const Header = ({ cart = [], favorites = [] }) => {
-<<<<<<< HEAD
-  const toggleMenu = () => {
-    setMenuOpen((prevMenuOpen) => !prevMenuOpen)
-  };
-  const [MenuOpen, setMenuOpen] = useState(false)
-
-  return (
-    <nav>
-      <div className="navbar">
-        <div className="navbar-item">
-          <span className="rotating-icon">
-            <FontAwesomeIcon icon={faBurger} />
-          </span>
-          <div className="logo">Hungry Wheels</div>
-        </div>
-        <ul className={`nav-links ${MenuOpen ? 'show' : ''}`}>
-=======
   const [flex, setFlex] = useState(false);
   const toggleMenu = () => {
     setFlex((prevState) => !prevState);
@@ -58,7 +41,6 @@ const Header = ({ cart = [], favorites = [] }) => {
       </nav>
       {flex && (
         <div className="nav-link">
->>>>>>> fa948c2814c5f17cfd29b2550e9ac7cc76dfbfd5
           <li>
             <Link to={"/"}>Home</Link>
           </li>
